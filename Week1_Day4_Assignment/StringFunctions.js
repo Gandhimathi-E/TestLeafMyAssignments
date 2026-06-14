2e5608d1638bf8a70b2a1ea46a1e9d9b0501e5ff
Gandhimathi-E <gandhi.perfect@gmail.com>
2026-06-14 12:42:29 +0530

    Add files via upload

--- Week1_Day4_Assignment/StringFunctions.js
+++ Week1_Day4_Assignment/StringFunctions.js
@@ -21,10 +21,12 @@ console.log("String Array is ",trimmedStrArray)
 console.log("String Array length is ",trimmedStrArray.length);
 
 console.log("The last wordlength is ",trimmedStrArray[(trimmedStrArray.length)-1].length);
-//*******************************************************************
+//********************************************************************
 
 //Example 3
-function isAnagram(str1,str2){
+
+
+/*function isAnagram(str1,str2){
     let sorterstr1 = str1.split("").sort().join("")
     let sorterstr2 = str2.split("").sort().join("")
     //sorterstr1 = str1.sort();
@@ -41,4 +43,23 @@ function isAnagram(str1,str2){
 let isAnagramBoolean1= isAnagram("listen", "silent")
 console.log("isAnagram : ",isAnagramBoolean1)
 let isAnagramBoolean2 = isAnagram('Hello', 'World')
-console.log("isAnagram : ",isAnagramBoolean2)
+console.log("isAnagram : ",isAnagramBoolean2)*/
+
+function isAnagram(){
+    let str1 = process.argv[2];
+    let str2 = process.argv[3];
+    console.log("process.argv[2] :",process.argv[2])
+    console.log("str1 :",str1)
+    let sorterstr1 = str1.split("").sort().join("")
+    let sorterstr2 = str2.split("").sort().join("")
+    //sorterstr1 = str1.sort();
+    console.log("Sorted String 1 is ",sorterstr1);
+    console.log("Sorted String 2 is ",sorterstr2);
+    if(sorterstr1 === sorterstr2){
+        return true;
+    } 
+    else{
+        return false;
+    }
+}
+console.log(isAnagram())
